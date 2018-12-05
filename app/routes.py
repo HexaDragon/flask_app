6a7db18bd8cdd14a065465e7eac50bf84ef7e2e6
h3xdrgN <rohanshirodkar29@gmail.com>
2018-12-04 22:53:44 -0500

    added render template import, and used the function pass index.html, title and user as return value

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 """different urls to route to"""
 
+from flask import render_template
 from app import app
-
 
 @app.route('/')
 @app.route('/index')
 def index():
-    """return the message string"""
-    return "Hello H3xDrgn..."
+    """return the homepage"""
+    user = {'username': 'H3xDrgn'}      # creating dictionary obj to store mock users
+    return render_template('index.html', title='Home',
+                           user=user)
